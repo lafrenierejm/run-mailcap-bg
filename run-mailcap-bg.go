8b04162c4acfdc2a505d9730a13a09b7bf00bd75
Joseph LaFreniere <joseph@lafreniere.xyz>
2017-06-21 19:37:59 -0500

    Add getRuntimeDir() to get runtime file directory

--- run-mailcap-bg.go
+++ run-mailcap-bg.go
@@ -3,7 +3,9 @@ package main
 import (
 	"flag"
 	"fmt"
+	"log"
 	"os"
+	"path/filepath"
 )
 
 // printUsage prints a ussage message for this program to stdout then exits
@@ -14,7 +16,26 @@ func printUsage() {
 	os.Exit(1)
 }
 
+// getRuntimeDir returns a path for program's runtime files that is compliant
+// with the XDG Base Directory Specification.  If $XDG_RUNTIME_DIR is unset in
+// the environment then os.TempDir() is used.
+func getRuntimeDir(program string) string {
+	var dir string
+
+	xdgRuntime := os.Getenv("XDG_RUNTIME_DIR")
+
+	if len(xdgRuntime) > 0 {
+		dir = filepath.Join(xdgRuntime, program)
+	} else {
+		dir = filepath.Join(os.TempDir(), program)
+	}
+
+	return dir
+}
+
 func main() {
+	const suffix = "run-mailcap-bg"
+
 	// Flags
 	isChild := flag.Bool("child", false, "parent PID")
 	flag.Parse()
@@ -24,6 +45,14 @@ func main() {
 		numArgs := len(os.Args)
 		if numArgs < 3 {
 			printUsage()
+		}
+
+		// Get the directory to copy the passed file to
+		dir := getRuntimeDir(suffix)
+		// Attempt to create the runtime directory
+		err := os.MkdirAll(dir, 0777)
+		if err != nil {
+			log.Fatalf("Could not create directory '%s': %v", dir, err)
 		}
 
 		// Exit sucessfully
